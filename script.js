5ab38b039dec68e25cd6a2763d1c7b38a4f3314c
WYT <tina.yiting.wei@gmail.com>
2021-04-08 14:20:36 +0800

    styled some more detailed(animation time function, circle background change, background image with trnasparent gradient) adjust time duration of one breathe)

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
+const circle = document.getElementById('circle');
 
 const totalTime = 10000;
-const breatheTime = totalTime/10*4.5;
+const breatheInTime = totalTime/10*4;
+const breatheOutTime = totalTime/10*5
 const holdTime = totalTime/10/2;
 
 function breatheAnimation(){
-    text.innerText = "Hold";
+    text.innerText = "Hold...";
 
     setTimeout(() => {
-        text.innerText = 'Breathe In';
+        text.innerText = 'Breathe In!';
         container.classList.remove('shrink');
         container.classList.add('grow');
+        circle.classList.add('in');
 
         setTimeout(() => {
             text.innerText = "Breathe Out!";
             container.classList.remove('grow');
             container.classList.add('shrink');
-            
+            circle.classList.remove('in');
+            circle.classList.add('out');
+
             setTimeout(() => {
-                text.innerText = "Hold";
-            },breatheTime)
-        }, breatheTime)
+                text.innerText = "Hold...";
+                circle.classList.remove('out');
+
+                setTimeout(() => {
+                    
+                },holdTime)
+
+            },breatheOutTime)
+        }, breatheInTime)
     }, holdTime);
 
 }
